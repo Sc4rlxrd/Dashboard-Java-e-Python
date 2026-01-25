9283d0355650dc139ff1484d94fbcd777cf7aa0d
Guilherme D. Santos <guilhermesantos2004gss@gmail.com>
2026-01-25 16:56:12 -0300

    refactor: Modifying the structure to read URLs from a text file so that it doesn't need to build the Docker image every time.

--- datacollector/src/main/java/com/scarlxrd/datacollector/model/runner/DataCollectorRunner.java
+++ datacollector/src/main/java/com/scarlxrd/datacollector/model/runner/DataCollectorRunner.java
@@ -7,6 +7,7 @@ import com.scarlxrd.datacollector.model.repository.ProductRepository;
 import com.scarlxrd.datacollector.model.service.ScraperService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import java.nio.file.Files;
 
 import java.io.File;
 import java.util.List;
@@ -24,28 +25,20 @@ public class DataCollectorRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+File file = new File("/app/urls.txt"); 
+    List<String> urls = new java.util.ArrayList<>(); 
 
-        List<String> urls = List.of(
-                "https://boadica.com.br/produtos/p206263/14t-512gb--12gb-titan-black-preto" ,
-                "https://boadica.com.br/produtos/p212724/15t--512gb--12gb-black",
-                "https://boadica.com.br/produtos/p211057/f7-12gb512gb-black-preto",
-                "https://boadica.com.br/produtos/p199610/iphone-15-128gb-desbloqueado-fabrica-preto",
-                "https://boadica.com.br/produtos/p205764/iphone-16-128gb-desbloqueado-fabrica-preto",
-                "https://boadica.com.br/pesquisa/compu_notebook/precos?ClasseProdutoX=1&CodCategoriaX=2&XF=1785",
-                "https://boadica.com.br/pesquisa/compu_notebook/precos?ClasseProdutoX=1&CodCategoriaX=2&XF=1785",
-                "https://boadica.com.br/pesquisa/compu_notebook/precos?ClasseProdutoX=1&CodCategoriaX=2&XF=1785",
-                "https://boadica.com.br/produtos/p186891/latitude-3420",
-                "https://boadica.com.br/produtos/p185513/latitude-5420",
-                "https://boadica.com.br/produtos/p197084/latitude-5430",
-                "https://boadica.com.br/produtos/p212956/ideapad-slim-3-15irh10",
-                "https://boadica.com.br/produtos/p201048/aspire-5-a515-57-727c",
-                "https://boadica.com.br/produtos/p213551/aspire-go-15-ag15-71p-76z8",
-                "https://boadica.com.br/produtos/p206808/nitro-v15-anv15-51-73e9",
-                "https://boadica.com.br/produtos/p203331/tuf-gaming-f15-fx507zc4-hn112-mecha-gray",
-                "https://boadica.com.br/produtos/p207535/thinkpad-t14-gen-4-21hesg6c00"
-             );
+    if (file.exists()) {
+        System.out.println("Carregando URLs do arquivo /app/urls.txt...");
+        urls = Files.readAllLines(file.toPath());
+    } else {
+        System.err.println("ERRO: Arquivo /app/urls.txt NÃO encontrado no container!");
+        urls.add("https://boadica.com.br/"); 
+    }
 
-        System.out.println("Iniciando a rodada de coleta para " + urls.size() + " itens...");
+    // Filtra linhas em branco para evitar erros no Selenium
+    urls.removeIf(String::isBlank);
+    System.out.println("Iniciando a rodada de coleta para " + urls.size() + " itens...");
         for (String url : urls) {
             try {
                 Thread.sleep(8000); 
